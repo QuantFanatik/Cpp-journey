04c593fcbd923bbd3b777d148353818c6c83ae54
David Huber <davidhuber@wl2s-233-169.unil.ch>
2024-12-11 20:16:40 +0100

    some if and else logic

--- logics and loops/if and else/if_else.cpp
+++ logics and loops/if and else/if_else.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
+#include <string>
 
 int main() {
-    int age;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    std::string sign;
+    bool interest;
 
-    if (age < 18) {
-        std::cout << "You are a minor. Enjoy your youth !! " << std::endl;
-    } else if (age >= 18 && age < 60) {
-        std::cout << "You are an adult. You should get that driving license ! " << std::endl;
+    std::cout << "Are you interested in astrology and in getting your horoscope? (1 = yes; 0 = no)" << std::endl;
+    std::cin >> interest;
+
+    if (interest == 1) {
+        std::cout << "Please enter your astrological sign:" << std::endl;
+        std::cin >> sign;
+
+        // Check the astrological sign using if-else if
+        if (sign == "Aries" || sign == "aries") {
+            std::cout << "You will be a millionaire" << std::endl;
+        } else if (sign == "Cancer" || sign == "cancer") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Libra" || sign == "libra") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Taurus" || sign == "taurus") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Leo" || sign == "leo") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Scorpio" || sign == "scorpio") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Gemini" || sign == "gemini") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Capricorn" || sign == "capricorn") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Aquarius" || sign == "aquarius") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Virgo" || sign == "virgo") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Pisces" || sign == "pisces") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else if (sign == "Sagittarius" || sign == "sagittarius") {
+            std::cout << "You will find your true love this week" << std::endl;
+        } else {
+            std::cout << "Invalid sign entered." << std::endl;
+        }
     } else {
-        std::cout << "You are a senior citizen. Is your back hurting ?" << std::endl;
+        std::cout << "Thank you for your answer, bye!" << std::endl;
     }
 
     return 0;
